4dff83d84441cc378550ef3744d00c0b8668ba39
Keneth180 <86987550+Keneth180@users.noreply.github.com>
2024-05-09 15:17:15 -0500

    Update stock.js

--- stock.js
+++ stock.js
@@ -1,4 +1,4 @@
-hlet stockProductos = [
+let stockProductos = [
     {id: 1, nombre: "Extension Negro Mixto Rubio",  cantidad: 1,  precio: 12000,  img: './resources/A1.jpeg'},
     {id: 2, nombre: "Pinza Veige",  cantidad: 1,  precio: 13000,  img: './resources/A2.jpeg'},
     {id: 3, nombre: "Reloj Dama y Accesorios",  cantidad: 1,  precio: 12000,  img: './resources/A3.jpeg'},
