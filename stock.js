9a871db9e4d8080d3fccb71d6d711c1894e8b370
Keneth180 <86987550+Keneth180@users.noreply.github.com>
2024-05-09 15:46:22 -0500

    Update stock.js

--- stock.js
+++ stock.js
@@ -1,6 +1,10 @@
 let stockProductos = [
     {id: 1, nombre: "Extension Negro Mixto Rubio",  cantidad: 1,  precio: 12000,  img: './resources/A1.jpeg'},
     {id: 2, nombre: "Pinza Veige",  cantidad: 1,  precio: 13000,  img: './resources/A2.jpeg'},
-    {id: 3, nombre: "Reloj Dama y Accesorios",  cantidad: 1,  precio: 12000,  img: './resources/A3.jpeg'},
+    {id: 3, nombre: "Reloj Dama Negro y Accesorios",  cantidad: 1,  precio: 12000,  img: './resources/A3.jpeg'},
     {id: 4, nombre: "Anillo 2 Cabezas",  cantidad: 1,  precio: 20000,  img: './resources/A4.jpeg'},
+    {id: 5, nombre: "Reloj Dama Verde y Accesorios",  cantidad: 1,  precio: 21000,  img: './resources/A5.jpeg'},
+    {id: 6, nombre: "Juego Aretes dorados",  cantidad: 1,  precio: 13000,  img: './resources/A6.jpeg'},
+    {id: 7, nombre: "Anillo Serpiente",  cantidad: 1,  precio: 12500,  img: './resources/A7.jpeg'},
+    {id: 8, nombre: "Extensión Castaño 18p",  cantidad: 1,  precio: 5800,  img: './resources/A8.jpeg'},
 ]
